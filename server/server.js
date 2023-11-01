d4a26501527f3032f0d466b38f3e133ae9347a9b
Mona Lisa <cordovamonica.a@gmail.com>
2023-10-31 20:40:11 -0500

    added missing dependencies

--- server/server.js
+++ server/server.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const db = require("./config/connection");
 const { ApolloServer } = require("apollo-server-express");
-const { expressMiddleware } = require("@apollo/server/express4");
 const { typeDefs, resolvers } = require("./schemas");
 const path = require("path");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 const startApolloServer = async () => {
   await server.start();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use("/graphql", expressMiddleware(server));
+
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
   }
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
